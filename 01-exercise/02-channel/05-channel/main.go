a6cdc8fbcf4ee817a9f17c7de0eb8e1fbe662cf0
Ricardo Mariato <ricardo.maricato@hotmail.com>
2022-04-19 23:59:14 -0300

    feat: channel ownership

---
+++ 01-exercise/02-channel/05-channel/main.go
@@ -0,0 +1,34 @@
+package main
+
+import "fmt"
+
+func main() {
+	// create channel owner goroutine, which create channel,
+	// return receive only channel to caller
+	// spins a goroutine, which
+	// writes data into channel and
+	// closes the channel when done.
+
+	owner := func() <-chan int {
+		ch := make(chan int)
+
+		go func() {
+			defer close(ch)
+			for i := 0; i < 5; i++ {
+				ch <- i
+			}
+		}()
+		return ch
+	}
+
+	consumer := func(ch <-chan int) {
+		// read values from channel
+		for v := range ch {
+			fmt.Printf("Received: %d\n", v)
+		}
+		fmt.Println("Done receiving!")
+	}
+
+	ch := owner()
+	consumer(ch)
+}
