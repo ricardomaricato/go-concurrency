84f3a45fbf33804e2b47ec794c6c58c09328905b
Ricardo Mariato <ricardo.maricato@hotmail.com>
2022-04-27 23:01:40 -0300

    feat: pipeline

---
+++ 02-exercise/01-pipeline/main.go
@@ -0,0 +1,53 @@
+package main
+
+import "fmt"
+
+// TODO: Build a Pipeline
+// generator() -> square() -> print
+
+// generator - convertes a list of integers to a channel
+func generator(nums ...int) <-chan int {
+	out := make(chan int)
+	go func() {
+		for _, n := range nums {
+			out <- n
+		}
+		close(out)
+	}()
+	return out
+}
+
+// square - receive on inbound channel
+// square the number
+// output on outbound channel
+func square(in <-chan int) <-chan int {
+	out := make(chan int)
+	go func() {
+		for n := range in {
+			out <- n * n
+		}
+		close(out)
+	}()
+	return out
+}
+
+func main() {
+	// set up the pipeline
+
+	// run the last stage of pipeline
+	// receive the values from square stage
+	// print each one, until channel is closed.
+
+	// esse código substitui o outro mais abaixo
+	//ch := generator(2, 3)
+	//
+	//out := square(ch)
+	//
+	//for n := range out {
+	//	fmt.Println(n)
+	//}
+
+	for n := range square(square(generator(2, 3))) {
+		fmt.Println(n)
+	}
+}
